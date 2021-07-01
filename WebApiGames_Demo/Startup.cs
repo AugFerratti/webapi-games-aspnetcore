8cb24245f176971b080092415b434b102a4f679e
Augusto Ferratti <augusto.ferratti@gmail.com>
2021-07-01 09:43:29 -0300

    ErrorHandling

--- WebApiGames_Demo/Startup.cs
+++ WebApiGames_Demo/Startup.cs
@@ -1,19 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using WebApiGames_Demo.Context;
+using WebApiGames_Demo.Extensions;
 using WebApiGames_Demo.Filters;
 using WebApiGames_Demo.Services;
 
@@ -63,6 +57,10 @@ namespace WebApiGames_Demo
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiGames_Demo v1"));
             }
+
+            //Error handling middleware
+            app.ConfigureExceptionHandler();
+
             //middlewares
             app.UseHttpsRedirection();
 
